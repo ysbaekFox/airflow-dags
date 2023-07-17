2b1f54da85e5260e73a1a919d50bc8af8eec6388
ysbaekFox <ysbaekFox@gmail.com>
2023-07-18 07:50:21 +0900

    fix

--- dags/rocket-launch.py
+++ dags/rocket-launch.py
@@ -26,43 +26,37 @@ download_launches = BashOperator(
         dag=dag,
 )
 
-# def _get_pictures():
-#     # Path() : Path 객체 생성
-#     # mkdir() - exist_ok=True : 폴더가 없을 경우 자동으로 생성
-#     pathlib.Path(TARGET_DIR).mkdir(parents=True, exist_ok=True)
-#
-#     # launches.json 파일에 있는 모든 그림 파일 download
-#     with open(JSON_PATH) as f:
-#         try:
-#             launches = json.load(f)
-#             image_urls = [launch['image'] for launch in launches['results']]
-#
-#             for i, image_url in enumerate(image_urls):
-#                 try:
-#                     response = requests.get(image_url)
-#                     image_filename = image_url.split('/')[-1]
-#                     target_file = f'{TARGET_DIR}/{image_filename}'
-#
-#                     with open(target_file, 'wb') as f:
-#                         f.write(response.content)
-#
-#                     print(f'Downloaded {image_url} to {target_file}')
-#                 except requests_exceptions.MissingSchema:
-#                     print(f'{image_url} appears to be an invalid URL.')
-#                 except requests_exceptions.ConnectionError:
-#                     print(f'Could not connect to {image_url}.')
-#         except KeyError as e:
-#             with open(JSON_PATH) as f:
-#                 print(json.load(f))  # ex : {'detail': 'Request was throttled. Expected available in 766 seconds.'}
-#             raise
-#
-#
-# get_pictures = PythonOperator(
-#     task_id="get_pictures",
-#     python_callable=_get_pictures,
-#     dag=dag
-# )
-#
+def _get_pictures():
+    # Path() : Path 객체 생성
+    # mkdir() - exist_ok=True : 폴더가 없을 경우 자동으로 생성
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
+
+    # launches.json 파일에 있는 모든 그림 파일 download
+    with open("/tmp/launches.json") as f:
+        launches = json.load(f)
+        image_urls = [launch['image'] for launch in launches['results']]
+
+        for i, image_url in enumerate(image_urls):
+            try:
+                response = requests.get(image_url)
+                image_filename = image_url.split('/')[-1]
+                target_file = f'"/tmp/images"/{image_filename}'
+
+                with open(target_file, 'wb') as f:
+                    f.write(response.content)
+                print(f'Downloaded {image_url} to {target_file}')
+            except requests_exceptions.MissingSchema:
+                print(f'{image_url} appears to be an invalid URL.')
+            except requests_exceptions.ConnectionError:
+                print(f'Could not connect to {image_url}.')
+
+
+get_pictures = PythonOperator(
+    task_id="get_pictures",
+    python_callable=_get_pictures,
+    dag=dag
+)
+
 notify = BashOperator(
     task_id="notify",
     bash_command='echo "Hello World"',
@@ -70,4 +64,4 @@ notify = BashOperator(
 )
 
 
-download_launches >> notify
+download_launches >> get_pictures >> notify
